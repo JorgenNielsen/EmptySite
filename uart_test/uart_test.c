e02841dc19d80497e0db418689fa89901b1efe5e
Jørgen Nielsen <JON@PC49.Homatic.local>
2024-01-11 09:17:09 +0100

    clean buffer

--- uart_test/uart_test.c
+++ uart_test/uart_test.c
@@ -11,11 +11,34 @@
 // git add .
 // git commit -m "comment"
 // git push --set-upstream https://github.com/JorgenNielsen/EmptySite master
+// root@46e322a49825:/workspace# source ./tpu-mlir/envsetup.sh
 
 // git clone https://github.com/JorgenNielsen/EmptySite  
+// root@46e322a49825:/workspace/duo-examples#  source envsetup.sh
+//
+// cd /workspace/jon-examples/EmptySite/uart_test
 // root@46e322a49825:/workspace/jon-examples/EmptySite/uart_test# git pull origin master
 // make
 // scp uart_test root@192.168.42.1:/root/
+
+
+// start
+/*
+	PS C:\Users\JON.PC49> docker ps
+	CONTAINER ID   IMAGE                  COMMAND       CREATED      STATUS         PORTS     NAMES
+	46e322a49825   sophgo/tpuc_dev:v3.1   "/bin/bash"   2 days ago   Up 6 seconds             DuoTPU
+	PS C:\Users\JON.PC49> docker exec -it 46e322a49825 /bin/bash
+	root@46e322a49825:/workspace# ls
+	duo-examples  jon-examples  tpu-mlir
+	root@46e322a49825:/workspace# cd jon-examples/
+	root@46e322a49825:/workspace/jon-examples# ls
+	EmptySite  lys_demo
+	root@46e322a49825:/workspace/jon-examples# cd EmptySite/
+	root@46e322a49825:/workspace/jon-examples/EmptySite# ls
+	readme.txt  uart_test
+	root@46e322a49825:/workspace/jon-examples/EmptySite# cd uart_test/
+	root@46e322a49825:/workspace/jon-examples/EmptySite/uart_test# git pull origin master
+*/
 
 int main() {
     struct wiringXSerial_t wiringXSerial = {115200, 8, 'n', 1, 'n'};
@@ -70,6 +93,7 @@ int main() {
         str_len = wiringXSerialDataAvail(cfd);
         if (str_len > 0) {
             i = 0;
+			memset(buf,0, sizeof(buf));
             while (str_len--)
             {
                 buf[i++] = wiringXSerialGetChar(cfd);
