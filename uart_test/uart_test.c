0d124a3e029a11b7eb1c19427207e82344165f47
Jørgen Nielsen <JON@PC49.Homatic.local>
2024-01-13 11:32:25 +0100

    last x2

--- uart_test/uart_test.c
+++ uart_test/uart_test.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#include <sys/stat.h>   // stat
+#include <stdbool.h>    // bool type
 
 #include <wiringx.h>
 
@@ -40,6 +46,21 @@
 	root@46e322a49825:/workspace/jon-examples/EmptySite/uart_test# git pull origin master
 */
 
+/*
+
+camera:
+netsh interface portproxy add v4tov4 listenaddress=192.168.0.27 listenport=8554 connectaddress=192.168.42.1 connectport=8554
+
+rtsp://192.168.0.27:8554/stream0
+*/
+
+void fileCheck(const char *fileName);
+
+bool file_exists (char *filename) {
+  struct stat   buffer;   
+  return (stat (filename, &buffer) == 0);
+}
+
 int main() {
     struct wiringXSerial_t wiringXSerial = {115200, 8, 'n', 1, 'n'};
     char buf[1024];
@@ -63,6 +84,15 @@ int main() {
 	printf("Duo UART started\n");
 	
 	char name[] = "foo.txt";
+	
+	fileCheck(name);
+	
+	if (file_exists(name)) {
+        printf("%s exists\n", name);
+	} else {
+        printf("%s does not exist\n", name);
+	}
+	
     FILE * fp;
     int fd = open(name, O_RDWR | O_CREAT, 0777);
 	if (fd != -1) {
@@ -105,4 +135,31 @@ int main() {
     wiringXSerialClose(cfd);
 
     return 0;
+}
+
+void fileCheck(const char *fileName){
+
+    if(!access(fileName, F_OK )){
+        printf("The File %s\t was Found\n",fileName);
+    }else{
+        printf("The File %s\t not Found\n",fileName);
+    }
+
+    if(!access(fileName, R_OK )){
+        printf("The File %s\t can be read\n",fileName);
+    }else{
+        printf("The File %s\t cannot be read\n",fileName);
+    }
+
+    if(!access( fileName, W_OK )){
+        printf("The File %s\t it can be Edited\n",fileName);
+    }else{
+        printf("The File %s\t it cannot be Edited\n",fileName);
+    }
+
+    if(!access( fileName, X_OK )){
+        printf("The File %s\t is an Executable\n",fileName);
+    }else{
+        printf("The File %s\t is not an Executable\n",fileName);
+    }
 }
